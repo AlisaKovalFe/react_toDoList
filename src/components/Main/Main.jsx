9699a42a24e95c70bfb6d11758f61a0f03aa5cbc
Alisa <alisakovalhome@mail.ru>
2023-06-30 17:41:26 +0300

    Add solution for LS in main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useState } from "react"
 import TaskList from '../TaskList/TaskList'
 import Form from '../Form/Form'
-import { useLocalStorage } from '../../hooks/useLS';
+
+let store = []
 
 function Main(props) {
 
     const [text, setText] = useState('')
-    const [list, setList] = useLocalStorage('tasks', ['нет у меня дел'])
-
+    const [list, setList] = useState([])
+    
     function handleSubmit(event) {
         event.preventDefault()
 
@@ -17,16 +18,34 @@ function Main(props) {
         id: Date.now(),
         status: false
         }
-
+        
         setList((prev) => [...prev, task])
         setText('')
+
+        store.push(task)     
+
+        if (localStorage.getItem('task') == null) {
+            localStorage.setItem('task', JSON.stringify(store))
+        } else if (localStorage.getItem('task') != null) {
+            localStorage.setItem('task', JSON.stringify(store))
+        }
     } 
 
     const handlerChange = (event) => {
 
-        let newList = list.map((el) => {
+        let newList = list.map((el, index) => {
         if (el.id.toString() === event.target.value) {
-            el.status === false ? el.status = true : el.status = false      
+            
+            el.status === false ? el.status = true : el.status = false    
+            
+            if (el.status === true) {
+                let storeCopy = [...store]
+                storeCopy.splice(index, 1)
+                localStorage.setItem('task', JSON.stringify(storeCopy))
+            } else {
+                localStorage.setItem('task', JSON.stringify(store))
+            }
+            
         }
         return el
         })
--- src/hooks/useLS.js
+++ src/hooks/useLS.js
@@ -1,29 +1,68 @@
 import { useEffect, useState } from "react"
 
 function getSavedValue(key, defaultValue) {
-    let savedValue = []
-    for (key = 0; localStorage.getItem(key) !== null; key++) {
-        const item = localStorage.getItem(key)
-        savedValue.push(item)
-    }
+    // let savedValue = []
+    // for (key = 0; localStorage.getItem(key) !== null; key++) {
+    //     const item = localStorage.getItem(key)
+    //     savedValue.push(item)
+    // }
     
-    return savedValue || defaultValue
+    // return savedValue || defaultValue
+
+    let currentValue
+
+        try {
+            currentValue = JSON.parse(
+                localStorage.getItem(key) || String(defaultValue)
+            )
+        } catch(err) {
+            currentValue = defaultValue
+        }
+        
+
+        return currentValue
 }
 
 export function useLocalStorage(key, initalValue) {
     const [value, setValue] = useState(getSavedValue(key, initalValue))
     useEffect(() => {
-        let str = ''
-        let count = 0
+        // let str = ''
+        // let count = 0
 
-        value.map((el) => {           
-            if (el.status === false) {
-                count++
-                str += `${count}) ${el.text} `
-            }
-            return el           
-        })
-        localStorage.setItem(key, str) 
+        // value.map((el) => {           
+        //     if (el.status === false) {
+        //         count++
+        //         str += `${count}) ${el.text} `
+        //     }
+        //     return el           
+        // })
+        localStorage.setItem(key, JSON.stringify(value)) 
     },  [key, value])
     return [value, setValue]
 }
+
+
+// import { useEffect, useState } from "react"
+
+// export function useLocalStorage(key, defaultValue) {
+//     const [value, setValue] = useState(() => {
+//         let currentValue
+
+//         try {
+//             currentValue = JSON.parse(
+//                 localStorage.getItem(key) || String(defaultValue)
+//             )
+//         } catch(err) {
+//             currentValue = defaultValue
+//         }
+
+//         return currentValue
+//     })
+
+//     useEffect(() => {
+//         localStorage.getItem(key, JSON.stringify(value))
+//     }, [value, key])
+
+//     return [value, setValue]
+// }
+
